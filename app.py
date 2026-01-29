32fea1aa138f0305eba868495bff535bab7c84df
competee <denisvitko55@gmail.com>
2026-01-29 20:03:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -111,9 +111,10 @@ def reset():
 
 # ------------------ START ------------------
 
+import os
+
 if __name__ == "__main__":
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
     )
